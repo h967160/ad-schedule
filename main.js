ac2849dde07e2e137916426af864a9c994acfae8
Huang <h96716@gmail.com>
2024-03-07 02:56:54 +0800

    feat: add dark mode toggle feature, and remove unnecessary CSS

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// action's popup effect
 const actionIcon = document.getElementById("action-icon");
 
 const toggleMenu = () => {
@@ -6,3 +7,40 @@ const toggleMenu = () => {
 };
 
 actionIcon.addEventListener("click", toggleMenu);
+
+// dark mode
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+const darkModeImg = document.getElementById("dark-mode-img");
+const logoImg = document.getElementById("logo-img");
+
+// Changing image URL according to mode
+const imageUrls = {
+  dark: {
+    logo: "https://raw.githubusercontent.com/ALPHACamp/WFE-data-table/0f97f3113bff18353154b8644eb0b31fff2a3bef/icons/logo-dark.svg",
+    darkMode:
+      "https://raw.githubusercontent.com/ALPHACamp/WFE-data-table/0f97f3113bff18353154b8644eb0b31fff2a3bef/icons/darkmode-off.svg",
+  },
+  light: {
+    logo: "https://raw.githubusercontent.com/ALPHACamp/WFE-data-table/0f97f3113bff18353154b8644eb0b31fff2a3bef/icons/logo.svg",
+    darkMode:
+      "https://raw.githubusercontent.com/ALPHACamp/WFE-data-table/0f97f3113bff18353154b8644eb0b31fff2a3bef/icons/darkmode-off.svg",
+  },
+};
+
+// Toggle between dark and light mode and update img
+const darkModeToggleHandler = (event) => {
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    logoImg.src = imageUrls.dark.logo;
+    darkModeImg.src = imageUrls.dark.darkMode;
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    logoImg.src = imageUrls.light.logo;
+    darkModeImg.src = imageUrls.light.darkMode;
+  }
+};
+darkModeToggle.addEventListener("change", darkModeToggleHandler);
+
+darkModeImg.addEventListener("click", () => {
+  darkModeToggle.click();
+});
